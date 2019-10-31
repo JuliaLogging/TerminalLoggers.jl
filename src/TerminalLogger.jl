8fec62ed75d9307814bb5d09fb38915d4348dbb6
Takafumi Arakaki <aka.tkf@gmail.com>
2019-10-30 23:21:59 -0700

    Remove "This file is a part of Julia"

--- src/TerminalLogger.jl
+++ src/TerminalLogger.jl
@@ -1,5 +1,3 @@
-# This file is a part of Julia. License is MIT: https://julialang.org/license
-
 """
     TerminalLogger(stream=stderr, min_level=Info; meta_formatter=default_metafmt,
                    show_limited=true, right_justify=0)
