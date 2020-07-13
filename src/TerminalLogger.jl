e98ba7bade760d0e5f47f8487c0ce06360ac77b7
Chris Foster <chris42f@gmail.com>
2020-07-13 13:13:06 +1000

    Support printing of exception stacks from Base.catch_stack() (#34)

--- src/TerminalLogger.jl
+++ src/TerminalLogger.jl
@@ -139,12 +139,26 @@ function format_message(message::AbstractString, prefix_width, io_context)
 end
 
 # Formatting of values in key value pairs
-showvalue(io, msg) = show(io, "text/plain", msg)
-function showvalue(io, e::Tuple{Exception,Any})
-    ex,bt = e
-    showerror(io, ex, bt; backtrace = bt!=nothing)
+function showvalue(io, key, msg)
+    if key === :exception && msg isa Vector && length(msg) > 1 && msg[1] isa Tuple{Exception,Any}
+        if VERSION >= v"1.2"
+            # Ugly code path to support passing exception=Base.catch_stack() We
+            # don't have a useful "Exception Stack" type to look for here until
+            # https://github.com/JuliaLang/julia/pull/29901 gets unstuck.
+            Base.show_exception_stack(io, msg)
+        else
+            # v1.0 and 1.1 don't have Base.show_exception_stack
+            show(io, "text/plain", msg)
+        end
+    else
+        show(io, "text/plain", msg)
+    end
 end
-showvalue(io, ex::Exception) = showerror(io, ex)
+function showvalue(io, key, e::Tuple{Exception,Any})
+    ex,bt = e
+    showerror(io, ex, bt; backtrace = bt!==nothing)
+end
+showvalue(io, key, ex::Exception) = showerror(io, ex)
 
 # Generate a text representation of all key value pairs, split into lines with
 # per-line indentation as an integer.
@@ -155,7 +169,7 @@ function format_key_value_pairs(kwargs, io_context)
     rows_per_value = max(1, dsize[1]÷(length(kwargs)+1))
     valio = IOContext(valbuf, IOContext(io_context, :displaysize=>(rows_per_value,dsize[2]-3)))
     for (key,val) in kwargs
-        showvalue(valio, val)
+        showvalue(valio, key, val)
         vallines = split(String(take!(valbuf)), '\n')
         if length(vallines) == 1
             push!(msglines, (2,SubString("$key = $(vallines[1])")))
